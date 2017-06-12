db3015b1d1f05b3a853ca4ba5ecdc0a78bc5999c
wangcheng <hzau.cs.wangcheng@gmail.com>
2017-06-13 00:24:05 +0800

    恢复了shell文件和一份草稿

---
+++ my_commands/create_post.sh
@@ -0,0 +1,18 @@
+categories=$1
+name=$2
+
+basePath='/home/Developer/Workspace/Blog/stormphoenix.github.io/_drafts/'
+fileName=$(date "+%Y-%m-%d-")
+time=$(date "+%Y-%m-%d %H:%M:%S +0800")
+
+fileName=${fileName}${name}'.markdown'
+
+touch ${basePath}${fileName}
+
+echo "---
+layout: post
+title:    \"${name}\"
+date:    ${time}
+categories:    ${categories}
+---
+write your content" >> ${basePath}${fileName}
